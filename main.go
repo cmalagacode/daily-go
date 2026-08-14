058580be68ebd9968ab7351657e9993173ae8648
Cristian Malaga <cmalaga720@gmail.com>
2026-08-13 23:13:16 -0400

    add items

--- main.go
+++ main.go
@@ -3,8 +3,22 @@ package main
 import (
 	"fmt"
 )
+var MAX int64 = 100
 
 func main() {
+	
 	fmt.Println("Hello World")
 	fmt.Println("Hello World")
+	myString := fmt.Sprintf("Hi %d", 11)
+	fmt.Println(myString)
+	for i := 0; i < 10; i++ {
+		fmt.Printf("This is iteration number # %d\n", i);
+	}
+	x :=  []int16 {
+		3, 4, 5,
+	}
+	for val := range x {
+		fmt.Println(val)
+	}
+	fmt.Println(MAX)
 }
